b8771879e9f9b87806b48d8bcabd8a09da74e8d9
koyaabe <koyatendo@gmail.com>
2021-10-22 18:07:27 +0900

    Add show image function to DstDetailViewController

--- View/DstDetailViewController.swift
+++ View/DstDetailViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class DstDetailViewController: UIViewController {
     @IBOutlet weak var dstName: UILabel!
+    @IBOutlet weak var dstImage: UIImageView!
+    @IBOutlet weak var dstName2: UILabel!
+    @IBOutlet weak var dstExit: UILabel!
+    @IBOutlet weak var dstAdress: UILabel!
     
     var received: [String] = []
     
@@ -11,12 +15,21 @@ class DstDetailViewController: UIViewController {
         navigationItem.backButtonDisplayMode = .minimal
         
         print(received)
-        print(received[1])
-
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
+        
+        // 目的地名
         dstName.text = received[1]
+        
+        // 目的地の画像
+        let image = UIImage(named: received[3])
+        dstImage.image = image
+        
+        dstName2.text = received[1]
+        
+        // 目的地に近い出口名
+        dstExit.text = received[0]
+        
+        dstAdress.text = received[4]
+        
     }
     
 }
